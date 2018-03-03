946073a71c96a45ebb3ce07730dd5d6568d442f9
Nicolas Giethlen <giethlen.nicolas@gmail.com>
2018-03-03 14:47:22 +0100

    [JS][CSS] New config option for enabling/disabling topbar search form

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-(function ($) {
-  console.log("main.js in action 😀");
+;(function($) {
+  console.log('main.js in action 😀')
 
   // CONFIGURATION
 
@@ -10,115 +10,111 @@
     stickyHeader: true,
     parallaxHero: true,
     rippleEffect: true,
-    rippleSelector: ".btn"
-  };
+    search: true,
+    rippleSelector: '.btn'
+  }
 
-
-  if (! window[CONFIG.namespace]) {
+  if (!window[CONFIG.namespace]) {
     // Create the app main object
-    window[CONFIG.namespace] = {};
+    window[CONFIG.namespace] = {}
     // Attach the configuration to it
-    Lightwords.CONFIG = CONFIG;
-
+    Lightwords.CONFIG = CONFIG
   } else {
-    console.error("OUCH! window." + CONFIG.namespace + " already exists!");
+    console.error('OUCH! window.' + CONFIG.namespace + ' already exists!')
   }
 
   // console.log('Lightwords.CONFIG:');
   // console.log(Lightwords.CONFIG);
 
-
   // DOM IS READY!
 
   $(function() {
-
     // CONFIGURATION BODY TAG CLASSES
 
-    var $body = $("body");
+    var $body = $('body')
 
     // Debug classes
 
     if (Lightwords.CONFIG.debug) {
-      $body.addClass("debug");
+      $body.addClass('debug')
 
       if (Lightwords.CONFIG.debuggers) {
-        Lightwords.CONFIG.debuggers.forEach(function (debug) {
-          $body.addClass("debug--" + debug);
-        });
+        Lightwords.CONFIG.debuggers.forEach(function(debug) {
+          $body.addClass('debug--' + debug)
+        })
       }
     }
 
-    if (Lightwords.CONFIG.stickyHeader) { $body.addClass("sticky-header"); }
-    if (Lightwords.CONFIG.parallaxHero) { $body.addClass("parallax-hero"); }
+    if (Lightwords.CONFIG.search) {
+      $body.addClass('search')
+    }
+    if (Lightwords.CONFIG.stickyHeader) {
+      $body.addClass('sticky-header')
+    }
+    if (Lightwords.CONFIG.parallaxHero) {
+      $body.addClass('parallax-hero')
+    }
 
     // TOGGLE HELPER
 
-    var $togglers = document.querySelectorAll('[data-toggle]');
+    var $togglers = document.querySelectorAll('[data-toggle]')
 
     Array.prototype.forEach.call($togglers, function($toggler) {
-
-      var $targets = document.querySelectorAll($toggler.getAttribute('data-toggle'));
-      var classname = $toggler.getAttribute('data-toggle-classname'); // TODO : name for data-attributes too
+      var $targets = document.querySelectorAll(
+        $toggler.getAttribute('data-toggle')
+      )
+      var classname = $toggler.getAttribute('data-toggle-classname') // TODO : name for data-attributes too
 
       Array.prototype.forEach.call($targets, function($target) {
-
         $toggler.addEventListener('click', function() {
+          toggleStuff(this, $target, classname)
+        })
+      })
 
-          toggleStuff(this, $target, classname);
-
-        });
-      });
-
-      var toggleStuff = function ($el, $target, classname) {
-
-        var activeClassname = 'active';
+      var toggleStuff = function($el, $target, classname) {
+        var activeClassname = 'active'
         if ($el.classList.contains(activeClassname)) {
-            $el.classList.remove(activeClassname);
-
-          } else {
-            $el.classList.add(activeClassname);
-          }
+          $el.classList.remove(activeClassname)
+        } else {
+          $el.classList.add(activeClassname)
+        }
 
         if ($target.classList.contains(classname)) {
-            $target.classList.remove(classname);
-
-          } else {
-            $target.classList.add(classname);
-          }
-      };
-    });
-
+          $target.classList.remove(classname)
+        } else {
+          $target.classList.add(classname)
+        }
+      }
+    })
 
     // PARALLAX JS
     // See: http://pixelcog.github.io/parallax.js/
 
-    var $hero = $('.parallax-hero .hero');
-    var $heroImg = $('.parallax-hero .hero > img');
+    var $hero = $('.parallax-hero .hero')
+    var $heroImg = $('.parallax-hero .hero > img')
 
-    $heroImg.css({display: "none"});
-    $hero.css({height: "56.25vw"});
+    $heroImg.css({ display: 'none' })
+    $hero.css({ height: '56.25vw' })
 
-    $hero.parallax(
-      {
-        imageSrc: $heroImg.attr("src"),
-        positionY: 'top',
-        positionX: 'center',
-        speed: 0.5,
-        androidFix: true
-      }
-    );
+    $hero.parallax({
+      imageSrc: $heroImg.attr('src'),
+      positionY: 'top',
+      positionX: 'center',
+      speed: 0.5,
+      androidFix: true
+    })
 
     // SEARCH HELPERS
 
-    var $search = $(".searchform");
-    var $searchInput = $(".searchform input[type='text']");
-    var $searchSubmit = $(".searchform input[type='submit']");
+    var $search = $('.searchform')
+    var $searchInput = $(".searchform input[type='text']")
+    var $searchSubmit = $(".searchform input[type='submit']")
 
     function setSearchSubmit(l) {
       if (l > 2) {
-        $searchSubmit.removeAttr("disabled");
+        $searchSubmit.removeAttr('disabled')
       } else {
-        $searchSubmit.attr("disabled", "disabled");
+        $searchSubmit.attr('disabled', 'disabled')
       }
     }
 
@@ -126,50 +122,47 @@
       setSearchSubmit($(this).val().length)
     }
 
-    setSearchSubmit($searchInput.val().length);
+    setSearchSubmit($searchInput.val().length)
 
-    $searchInput.on("keyup", checkSearchValue);
+    $searchInput.on('keyup', checkSearchValue)
     // checkSearchValue();
-
 
     // RIPPLE EFFECT
 
     if (Lightwords.CONFIG.rippleEffect) {
+      $(Lightwords.CONFIG.rippleSelector).addClass('ripple')
 
-      $(Lightwords.CONFIG.rippleSelector).addClass("ripple");
-
-      $(".ripple").click(function(e){
-        var rippler = $(this);
+      $('.ripple').click(function(e) {
+        var rippler = $(this)
 
         // create .ink element if it doesn't exist
-        if(rippler.find(".ink").length == 0) {
-          rippler.append("<span class='ink'></span>");
+        if (rippler.find('.ink').length == 0) {
+          rippler.append("<span class='ink'></span>")
         }
 
-        var ink = rippler.find(".ink");
+        var ink = rippler.find('.ink')
 
         // prevent quick double clicks
-        ink.removeClass("animate");
+        ink.removeClass('animate')
 
         // set .ink diametr
-        if(!ink.height() && !ink.width())
-        {
-          var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
-          ink.css({height: d, width: d});
+        if (!ink.height() && !ink.width()) {
+          var d = Math.max(rippler.outerWidth(), rippler.outerHeight())
+          ink.css({ height: d, width: d })
         }
 
         // get click coordinates
-        var x = e.pageX - rippler.offset().left - ink.width()/2;
-        var y = e.pageY - rippler.offset().top - ink.height()/2;
+        var x = e.pageX - rippler.offset().left - ink.width() / 2
+        var y = e.pageY - rippler.offset().top - ink.height() / 2
 
         // set .ink position and add class .animate
-        ink.css({
-          top: y+'px',
-          left:x+'px'
-        }).addClass("animate");
+        ink
+          .css({
+            top: y + 'px',
+            left: x + 'px'
+          })
+          .addClass('animate')
       })
     }
-
-  });
-
-}(jQuery))
+  })
+})(jQuery)
