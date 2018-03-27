e8dffc4cfca466b5ae765bc8ebf7f6b34572e2c9
Nicolas Giethlen <giethlen.nicolas@gmail.com>
2018-03-27 19:53:13 +0200

    [TWIG] Unified index, archive and author templates + removed tease partial

--- archive.php
+++ archive.php
@@ -18,18 +18,18 @@ $templates = array( 'archive.twig', 'index.twig' );
 
 $context = Timber::get_context();
 
-$context['title'] = 'Archive';
+$context['title'] = 'Articles';
 
 if ( is_day() ) {
-	$context['title'] = 'Archive: '.get_the_date( 'D M Y' );
+	$context['title'] = 'Articles par date : '.get_the_date( 'D M Y' );
 } else if ( is_month() ) {
-	$context['title'] = 'Archive: '.get_the_date( 'M Y' );
+	$context['title'] = 'Articles par date : '.get_the_date( 'M Y' );
 } else if ( is_year() ) {
-	$context['title'] = 'Archive: '.get_the_date( 'Y' );
+	$context['title'] = 'Articles par date : '.get_the_date( 'Y' );
 } else if ( is_tag() ) {
-	$context['title'] = single_tag_title( '', false );
+	$context['title'] = 'Articles avec le tag '.single_tag_title( '', false );
 } else if ( is_category() ) {
-	$context['title'] = single_cat_title( '', false );
+	$context['title'] = 'Articles dans la catégorie '.single_cat_title( '', false );
 	array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
 } else if ( is_post_type_archive() ) {
 	$context['title'] = post_type_archive_title( '', false );
--- author.php
+++ author.php
@@ -16,7 +16,7 @@ $context['posts'] = Timber::get_posts();
 if ( isset( $wp_query->query_vars['author'] ) ) {
 	$author = new TimberUser( $wp_query->query_vars['author'] );
 	$context['author'] = $author;
-	$context['title'] = 'Author Archives: ' . $author->name();
+	$context['title'] = 'Articles de ' . $author->name();
 }
 
 Timber::render( array( 'author.twig', 'archive.twig' ), $context );
