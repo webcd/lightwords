739668217a98c48bfe09fab759ae804b47866375
Nicolas Giethlen <giethlen.nicolas@gmail.com>
2018-10-07 14:41:44 +0200

    [Fix][JS] Image wall resizing with lazy load

--- src/js/lib/image-wall.js
+++ src/js/lib/image-wall.js
@@ -1,36 +1,50 @@
 ;(function($) {
 
+  function setImageSize($item) {
+
+    var itemWidth = $item.getBoundingClientRect().width
+    var itemHeight = $item.getBoundingClientRect().height
+    var itemRatio = itemWidth / itemHeight
+
+    var $itemImg = $item.firstChild // Risky!
+    var itemImgWidth = $itemImg.getBoundingClientRect().width
+    var itemImgHeight = $itemImg.getBoundingClientRect().height
+    var itemImgRatio = itemImgWidth / itemImgHeight
+
+    if (itemRatio < itemImgRatio) {
+      $itemImg.style.width = Math.round(itemHeight / itemImgHeight * itemImgWidth) + 'px'
+      $itemImg.style.height = Math.round(itemHeight) + 'px'
+
+    } else {
+      $itemImg.style.width = Math.round(itemWidth) + 'px'
+      $itemImg.style.height = Math.round(itemWidth / itemImgWidth * itemImgHeight) + 'px'
+    }
+  }
+
   function setImagesSize($wallItems) {
 
     Array.prototype.forEach.call($wallItems, function($item) {
-      var itemWidth = $item.getBoundingClientRect().width
-      var itemHeight = $item.getBoundingClientRect().height
-      var itemRatio = itemWidth / itemHeight
-  
-      var $itemImg = $item.firstChild // Risky!
-      var itemImgWidth = $itemImg.getBoundingClientRect().width
-      var itemImgHeight = $itemImg.getBoundingClientRect().height
-      var itemImgRatio = itemImgWidth / itemImgHeight
-  
-      if (itemRatio < itemImgRatio) {
-        $itemImg.style.width = Math.round(itemHeight / itemImgHeight * itemImgWidth) + 'px'
-        $itemImg.style.height = Math.round(itemHeight) + 'px'
-  
-      } else {
-        $itemImg.style.width = Math.round(itemWidth) + 'px'
-        $itemImg.style.height = Math.round(itemWidth / itemImgWidth * itemImgHeight) + 'px'
-      }
+      setImageSize($item)
     })
   }
 
   $(function() {
-    var $wallItems = document.querySelectorAll('.wall .gallery .gallery-item .gallery-icon a')
+    var selector = '.wall .gallery .gallery-item .gallery-icon a'
+    var $wallItems = document.querySelectorAll(selector)
 
-    setImagesSize($wallItems)
+    // TODO: This break images resizing with lazyload!!!
+    // setImagesSize($wallItems)
 
     window.addEventListener("resize", function(){
       setImagesSize($wallItems)
-    });
+    })
+
+    // Lazyload
+    $items = $(selector)
+    $items.on('lazyload', function (e, $el) {
+      console.log('LAZY', $el[0].parentNode)
+      setImageSize($el[0].parentNode)
+    })
 
     console.log('image-wall.js is loaded')
   })
