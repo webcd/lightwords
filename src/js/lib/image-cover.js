353f6e733dee592184428d2f992652bc13242d3b
Nicolas Giethlen <giethlen.nicolas@gmail.com>
2018-11-03 18:23:46 +0100

    [CSS][JS] Automatic caption lightbox initialization

--- src/js/lib/image-cover.js
+++ src/js/lib/image-cover.js
@@ -11,12 +11,14 @@ const imageCover = (() => {
         $itemImg.addEventListener("load", function() {
           this.style.transform = "translateX(100000rem)"
 
-          const URL = this.getAttribute("src")
+          const $parent = this.parentNode
+          const url = this.getAttribute("src")
 
-          this.parentNode.style.backgroundImage = `url("${URL}")`
-          this.parentNode.style.backgroundSize = "cover"
-          this.parentNode.style.backgroundRepeat = "no-repeat"
-          this.parentNode.style.backgroundPosition = "50% 50%"
+          $parent.style.backgroundImage = `url("${url}")`
+          $parent.style.backgroundSize = "cover"
+          $parent.style.backgroundRepeat = "no-repeat"
+          $parent.style.backgroundPosition = "50% 50%"
+          $parent.style.display = "block"
         })
       }
     }
@@ -26,7 +28,6 @@ const imageCover = (() => {
   const $items = document.querySelectorAll(selector)
 
   if ($items.length) {
-    // console.warn("IMAGE COVERS", $items)
     setImagesSize($items)
   }
 })()
--- src/js/lib/lightbox.js
+++ src/js/lib/lightbox.js
@@ -3,7 +3,31 @@ import SmartPhoto from "smartphoto"
 // LIGHTBOX
 
 const lightbox = (() => {
+
+  // Prepare meta datas
+  const setImagesDatas = $items => {
+    for (let $item of $items) {
+      const $itemImg = $item.querySelector("img[src]")
+
+      if ($itemImg) {
+        // Auto-set caption from img's alt attribute
+        const alt = $itemImg.getAttribute("alt")
+
+        if (alt && alt !== "") {
+          $item.setAttribute("data-caption", alt)
+        }
+
+        // TODO: thumbnails broken with lazyload!
+      }
+    }
+  }
+
   const selector = ".lightbox, .wall .gallery a"
+  const $items = document.querySelectorAll(selector)
+
+  if ($items.length) {
+    setImagesDatas($items)
+  }
 
   // SmartPhoto
   // See: https://appleple.github.io/SmartPhoto/doc.html
@@ -13,7 +37,7 @@ const lightbox = (() => {
   })
 
   // const updateCounter = () => {
-    
+
   //   window.setTimeout(() => {
   //     const $count = document.querySelector(".smartphoto-count")
   //     const countText = $count.textContent
@@ -24,15 +48,14 @@ const lightbox = (() => {
   //     while ($count.firstChild) {
   //       $count.removeChild($count.firstChild)
   //     }
-  
+
   //     $count.innerHTML = `<span class="counter-current">${countCurrent}</span>/<span class="counter-total">${countTotal}</span>`
   //   }, 100)
   // }
 
   // mySmartPhoto.on("open", updateCounter)
-  
-  // mySmartPhoto.on("change", updateCounter)
 
+  // mySmartPhoto.on("change", updateCounter)
 })()
 
 export default lightbox
