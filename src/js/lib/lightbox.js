792dbfc070032e9422696ee9c28308d2b4e728b5
Nicolas Giethlen <giethlen.nicolas@gmail.com>
2018-11-03 16:52:34 +0100

    [CSS][JS] Better lightbox (aka SmartPhoto) styling

--- src/js/lib/lightbox.js
+++ src/js/lib/lightbox.js
@@ -1,8 +1,37 @@
 import SmartPhoto from "smartphoto"
 
-const lightbox = (() => {
+// LIGHTBOX
 
-  new SmartPhoto(".lightbox, .wall .gallery a")
+const lightbox = (() => {
+  const selector = ".lightbox, .wall .gallery a"
+
+  // SmartPhoto
+  // See: https://appleple.github.io/SmartPhoto/doc.html
+
+  const mySmartPhoto = new SmartPhoto(selector, {
+    // resizeStyle: 'fill'
+  })
+
+  // const updateCounter = () => {
+    
+  //   window.setTimeout(() => {
+  //     const $count = document.querySelector(".smartphoto-count")
+  //     const countText = $count.textContent
+  //     const countCurrent = countText.split('/')[0]
+  //     const countTotal = countText.split('/')[1]
+  //     console.log("count", countCurrent, countTotal)
+
+  //     while ($count.firstChild) {
+  //       $count.removeChild($count.firstChild)
+  //     }
+  
+  //     $count.innerHTML = `<span class="counter-current">${countCurrent}</span>/<span class="counter-total">${countTotal}</span>`
+  //   }, 100)
+  // }
+
+  // mySmartPhoto.on("open", updateCounter)
+  
+  // mySmartPhoto.on("change", updateCounter)
 
 })()
 
