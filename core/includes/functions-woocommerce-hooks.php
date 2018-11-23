a161047253f3f6baf73e97aa881594398a457c76
Alexandre Rochant <alexandre.rochant@plus-que-pro.fr>
2018-11-23 16:28:21 +0100

    [Woo][PHP][TPL][CSS] Badges: on-sale and featured

--- core/includes/functions-woocommerce-hooks.php
+++ core/includes/functions-woocommerce-hooks.php
@@ -16,7 +16,8 @@ remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
 // remove_action( 'woocommerce_before_single_product', 'wc_print_notices', 10 );
 
 // Remove product on-sale
-// remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10 );
+remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
+remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10 );
 // Remove product images
 // remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
 
